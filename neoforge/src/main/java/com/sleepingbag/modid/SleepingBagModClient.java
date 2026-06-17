d54d3ff1638e4620d92c0d032827a21872e647aa
Ben Henderson <benhenderson@Bens-MacBook-Air.local>
2026-06-17 19:27:58 -0400

    Fix NeoForge 26.x launcher compatibility

--- neoforge/src/main/java/com/sleepingbag/modid/SleepingBagModClient.java
+++
@@ -1,9 +0,0 @@
-package com.sleepingbag.modid;
-
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.common.Mod;
-
-@Mod(value = SleepingBagMod.MODID, dist = Dist.CLIENT)
-public final class SleepingBagModClient {
-    public SleepingBagModClient() {}
-}
